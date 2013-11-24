653e3fbddab6f43c60c5d947813a7efe55c14448
jack <jackmuratore@gmail.com>
2013-11-24 14:13:26 -0500

    * Add contact php file

---
+++ handle-contact.php
@@ -0,0 +1,15 @@
+<?php
+
+$first_name = $_GET["first-name"];
+$last_name = $_GET["last-name"];
+$email = $_GET["email"];
+$phone = $_GET["phone-number"];
+$comments = $_GET["comments"];
+
+sprintf($output, "Name:%s %s\nEmail:%s\nPhone:%s\nComments\n%s",
+    $first_name, $last_name, $email, $phone, $comments);
+
+mail( "jackmuratore@gmail.com",
+    "From My WebSite",
+    $output);
+?>
