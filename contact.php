2429bc4873984f1cea9ecdccaeed62baa84f20bd
jack <jackmuratore@gmail.com>
2013-11-24 14:11:56 -0500

    * Attempt to fix email problem with server

--- contact.php
+++ contact.php
@@ -6,9 +6,9 @@ contact form below.
 <form id='contact-me' class='contact-form'>
 <input placeholder='First name' type='text' required name='first-name'>
 <input placeholder='Last name' type='text' name='last-name'>
-<input placeholder='Email' type='email' required name='last-name'>
+<input placeholder='Email' type='email' required name='email'>
 <input placeholder='Phone' type='text' name='phone-number'>
-<textarea placeholder='Comments or Questions' rows='4' cols='70'>
+<textarea placeholder='Comments or Questions' name='comments' rows='4' cols='70'>
 </textarea>
 </form>
 <button id='send-btn'>Send</button>
@@ -32,17 +32,25 @@ var send_invalid_form_msg = function(msg)
 }
 
 $(document).ready(function() {
-    $('#send-btn').button();
-    $('#cancel-btn').button();
+    $('#send-btn').button({
+        icons: {secondary: 'ui-icon-circle-check'}
+    });
+    $('#cancel-btn').button({
+        icons: {secondary: 'ui-icon-circle-close'}
+    });
 
     $('#send-btn').click(function() {
-        var empty_field = "";
-        empty_field = validate_form();
+        var empty_field = "",
+            data = {};
+        //empty_field = validate_form();
         if (empty_field !== "") {
             send_invalid_form_msg(empty_field);
             return;
         }
-        $('#contact-form').submit();
+        data = $('#contact-me').serialize();
+        $.get('handle-contact.php', data, function(data) {
+           console.log(data); 
+        });
     });
 });
 </script>
