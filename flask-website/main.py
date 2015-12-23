91139766dd260b1aacb9000cb96d5508b5b64507
banjocat <jackmuratore@gmail.com>
2015-12-23 12:16:08 -0500

    Set up migration to flask.py

--- flask-website/main.py
+++ flask-website/main.py
@@ -1,22 +1,25 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
-
 
 
 
 @app.route('/')
 def home():
-    return 'Home'
+    return render_template('index.html')
 
-@app.route('/boxinvader')
-def boxinvader():
-    return 'Demos'
 
 @app.route('/conway')
 def conway():
-    return 'Conway'
+    return render_template('conway.html')
 
+@app.route('/biglife')
+def bigconway():
+    return render_template('biglife.html')
 
+@app.route('/boxinvader')
+def boxinvader():
+    return render_template('boxinvader.html')
 
 if __name__ == '__main__':
     app.debug = True
