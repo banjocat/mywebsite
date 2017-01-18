ad2c4f0a26ca3619362deecc25ac17834d03cb83
banjocat <jackmuratore@gmail.com>
2017-01-17 23:24:40 -0500

    fab deploy now builds and pushes image

--- fabfile.py
+++ fabfile.py
@@ -3,11 +3,11 @@ from fabric.api import run, put, env, hosts, cd, local
 env.user = 'root'
 env.port = 2222
 
-def push():
-    local('docker-compose push')
 
 @hosts('jackmuratore.com')
 def deploy():
+    local('docker-compose build')
+    local('docker-compose push')
     run('mkdir -p /app/jack/')
     put('./production-compose.yml', '/app/jack/docker-compose.yml')
     with cd('/app/jack'):
